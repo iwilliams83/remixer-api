cb870e5f88c7a032a1ade2c452dbdf663abfe667
Indira Williams <indirajamurath@gmail.com>
2018-09-03 09:03:53 -0400

    switch over to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_08_01_213153) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "songs", force: :cascade do |t|
     t.string "name"
     t.string "artist_name"
